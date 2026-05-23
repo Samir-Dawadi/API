389a25e29483060cd771adff9d508526fc6b4b6b
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-23 13:17:29 +0545

    Error handling continue..

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -5,7 +5,7 @@ class AuthController {
     useRegister = (req: Request, res: Response , next:NextFunction) => { }
 
     loginUser = (req: Request, res: Response , next:NextFunction) => {
-        res.json({
+        res.json({                                               //success ra failure duitai ko lai pathauna res.json nai hunxa tara failure i.e error ko lagi chai next vitra hunxa 
             data: "Cannot login check your username and password again",
             message: "fail",
             meta: null
@@ -13,7 +13,7 @@ class AuthController {
     }
 
     activeUser=(req: Request, res: Response , next:NextFunction) => { 
-        next({})
+        next({})     //yo next vitra ahko params haru chai ava error le ligxa that is declared in the errorhandlingmiddleware
     }
     resendActivationCode=(req: Request, res: Response , next:NextFunction) => { }
     forgetPassword=(req: Request, res: Response , next:NextFunction) => { }
--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -1,7 +1,11 @@
-import type{ NextFunction , Request ,Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
-export default function ErrorHandlingMiddleware(error:Error, req:Request,res:Response,next:NextFunction){
+export default function ErrorHandlingMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
 
-
+    res.json({
+        error: null,                                   //success ko case ma yeha data hunthe tara yo error handling ho so error aru same as in res.json for the success case
+        message: "Accessed denied",
+        meta: null
+    })
 
 }
