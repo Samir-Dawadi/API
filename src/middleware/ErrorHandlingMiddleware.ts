73c5236a5f8ab3308816cc91b7ef231a4df5adbd
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-23 14:45:47 +0545

    Basic formatting of error handling middleware

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -3,16 +3,20 @@ import type { NextFunction, Request, Response } from "express";
 
 
 export interface CustomError extends Error {
-    detail?: Record<string, string | Array<Record<string, string>>> | null | number
+    detail?: Record<string, string | Array<Record<string, string>>> | null | number,
+    code?: number | string
 }
 
 export default function ErrorHandlingMiddleware(error: CustomError, req: Request, res: Response, next: NextFunction) {         //Error chai yeauta interface ho 
 
     let detail = error.detail || error.stack || null
+    let code = error.code || 500
+    let message = error.message || "Internal server Error"
 
-    res.status(200).json({                             //by defaullt res.json is always 2xx i.e success
+
+    res.status(+code).json({                             //by defaullt res.json is always 2xx i.e success
         error: detail,                                   //success ko case ma yeha data hunthe tara yo error handling ho so error aru same as in res.json for the success case
-        message: "Accessed denied",
+        message: message,
         meta: null
     })
 
