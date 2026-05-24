180964bc18424adb99ee3f5e94ecbe18463f7ca5
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-24 20:53:53 +0545

    Radix value

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,21 @@
 import http from "http";
 import app from './app';
 
-// const PORT:number = parseInt(process.env.PORT || '9005' , 10) 
+// const PORT:number = parseInt(process.env.PORT || '9005' , 10)        10 chai radix ho i.e Yo number kun language/system ma lekheko ho?" vanera computer lai bujhaune kura ho. 10->decimal so 9*10^3 + 9*10^2 + 9*10^1 + 9*10^0 =9005
 const PORT: number = 9005;
 const server = http.createServer(app);
 
-server.listen(PORT, () => { 
-    console.log("Server running on http://localhost:"+PORT)
+server.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT)
     console.log("Press ctrl + c to disconnect your server")
 })
 
-server.on("error",(err:NodeJS.ErrnoException)=>{
-    if(err.code === "EADDRINUSE"){
+server.on("error", (err: NodeJS.ErrnoException) => {
+    if (err.code === "EADDRINUSE") {
         console.error(`port ${PORT} already in use . Try with different than ${PORT}`)
     }
-    else{
-        console.error("Server Error :" , err.message)
+    else {
+        console.error("Server Error :", err.message)
     }
 
     process.exit(1)
