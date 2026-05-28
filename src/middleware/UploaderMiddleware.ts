c1de63fcc43a0dd302e642178f4425680d08e05a
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-28 10:33:41 +0545

    Multer setup

--- src/middleware/UploaderMiddleware.ts
+++ src/middleware/UploaderMiddleware.ts
@@ -4,18 +4,19 @@ import path from "node:path"
 import fs from "fs"
 
 const uploader = (dir: string = '/') => {
+    //storage configuration
     const myStorage = multer.diskStorage({
         destination: (req, file, cb) => {
             try {
-                const __dirname = process.cwd();
+                const __dirname = process.cwd();                                             //path of file upto API                                
                 console.log(__dirname)
-                const filePath = path.join(__dirname, `/public/uploads${dir}`)
+                const filePath = path.join(__dirname, `/public/uploads${dir}`)              //ka nira rakhni file
                 if (!fs.existsSync(filePath)) {
                     fs.mkdirSync(filePath, { recursive: true })
                 }
                 cb(null, filePath)
             } catch (exception) {
-                console.log(new Error ("Storage cannot be configured"))
+                console.log(new Error("Storage cannot be configured"))
             }
         },
         filename: (req, file, cb) => {
@@ -27,6 +28,7 @@ const uploader = (dir: string = '/') => {
         }
     })
 
+    //file validation config
     const fileFilter = (req: Request, file: Express.Multer.File, cb: (obj: Error | null, arg?: boolean) => void) => {
         const ext = file.originalname.split(".").pop() as string;
         const allowedExts = ['jpg', 'png']
@@ -40,9 +42,9 @@ const uploader = (dir: string = '/') => {
     }
 
     return multer({
-        storage: myStorage,
-        fileFilter: fileFilter,
-        limits: {
+        storage: myStorage,                                                         //where do i want to store my
+        fileFilter: fileFilter,                                                      //kasto khalko file haru upload garxu vanni kurako validation , file kaile pani req.body bata garna mildaina so hami yeha bata validate garxam
+        limits: {                                                                       //file ko size validation
             fileSize: 7 * 1024 * 1024
         }
     })
