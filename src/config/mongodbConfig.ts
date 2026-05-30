3f982c5b7c894f342b8db7a8107251b159451c83
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-30 18:50:11 +0545

    mongodbconfig updated again

--- src/config/mongodbConfig.ts
+++ src/config/mongodbConfig.ts
@@ -7,14 +7,14 @@ import { MongoConfig } from "./appconfig";
 (async () => {                                                      //always async & if mpngodb is imported in any file then this will automatically executed with the help of immediately invoked function expression (IIFE)
     try {
         await mongoose.connect(MongoConfig.url as string, {
-            dbName: MongoConfig.name,
-            autoCreate: true,                                           //if db xaina vani auto banaidinxa
-            autoIndex: true
+            dbName: MongoConfig.name,                                   //this is the name of the database to use. If the database does not exist, MongoDB will create it automatically when you first write data to it.
+            autoCreate: true,                                           //this option tells Mongoose to automatically create the database if it doesn't exist when you connect to it. This can be useful in development environments where you want to quickly set up a new database without having to manually create it first. However, in production environments, it's generally recommended to create the database manually and set appropriate permissions to ensure better security and control over your data.
+            autoIndex: true                                             // this option tells Mongoose to automatically create indexes defined in your schemas when you connect to the database. This can be useful for development and testing environments, as it ensures that your indexes are always up to date without requiring manual intervention. However, in production environments, it's often recommended to disable autoIndex and manage indexes manually to avoid potential performance issues during application startup, especially if you have a large dataset or complex indexing requirements.
         })
         console.log("Database connected successfully")
     } catch (exception) {
         console.error("Error connecting mongodb")
         console.log(exception)
-        process.exit(1)
+        process.exit(1)                                                // this line is used to exit the Node.js process with a non-zero exit code, indicating that an error occurred. In this case, if there is an error connecting to MongoDB, the application will log the error message and then terminate with an exit code of 1, which is a common convention for indicating that an error occurred. This helps to prevent the application from running in an unstable state when it cannot connect to the database, and allows for better error handling and debugging.
     }
 })()
