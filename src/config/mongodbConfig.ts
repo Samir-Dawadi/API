a2858a3d45b9d281e54b45ff962573b65006ce57
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-30 18:48:28 +0545

    mongodbconfig updated

--- src/config/mongodbConfig.ts
+++ src/config/mongodbConfig.ts
@@ -1,5 +1,5 @@
-// import dns from 'dns';
-// dns.setServers(['8.8.8.8', '8.8.4.4']);
+import dns from 'dns';                    //this will set the DNS servers to Google's public DNS servers
+dns.setServers(['8.8.8.8', '8.8.4.4']);         //this is done to ensure that the application can resolve domain names correctly, especially in environments where the default DNS servers may not be reliable or accessible. By using Google's public DNS servers, the application can improve its ability to resolve domain names and connect to external services, such as MongoDB, without encountering DNS-related issues.
 
 import mongoose from "mongoose";
 import { MongoConfig } from "./appconfig";
